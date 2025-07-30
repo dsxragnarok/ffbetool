8a8c999c44693dd1c977f721ab4ed2174d72daa1
Kevin Phung <phung.kevin@gmail.com>
2025-07-29 19:39:29 -0600

    refactor: impl cgs processing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,17 +11,15 @@ fn main() -> std::result::Result<(), String> {
 
     let unit_id: u32 = args[1].parse().expect("unit_id should be numerical value");
     let input_path = &args[2];
-    let anim_name = if args.len() < 4 {
-        None
-    } else {
-        Some(&args[3])
-    };
+    let anim_name = if args.len() < 4 { None } else { Some(&args[3]) };
 
     println!("ffbetool on {unit_id} cgg-file:[{input_path}]");
     let frames = match cgg::read_file(unit_id, input_path) {
         Ok(reader) => {
-            let frames: Vec<cgg::FrameParts> = reader.lines().enumerate().filter_map(|(row, line_result)| {
-                match line_result {
+            let frames: Vec<cgg::FrameParts> = reader
+                .lines()
+                .enumerate()
+                .filter_map(|(row, line_result)| match line_result {
                     Ok(line) => {
                         let parts = cgg::process(&line, row);
                         parts
@@ -29,9 +27,9 @@ fn main() -> std::result::Result<(), String> {
                     Err(err) => {
                         eprintln!("failed to read line {row}: {err}");
                         None
-                    },
-                }
-            }).collect();
+                    }
+                })
+                .collect();
             frames
         }
         Err(err) => {
@@ -48,20 +46,35 @@ fn main() -> std::result::Result<(), String> {
     let src_img = ffbetool::image::load_source_image(unit_id, input_path);
 
     match anim_name {
-        Some(anim_name) => {
-            match cgs::read_file(unit_id, anim_name, input_path) {
-                Ok(reader) => {
-
-                },
-                Err(err) => {
-                    eprintln!("failed to process cgs file: {err}");
-                    return Err(err.to_string());
-                }
+        Some(anim_name) => match cgs::read_file(unit_id, anim_name, input_path) {
+            Ok(reader) => {
+                let cgs_frames_meta: Vec<cgs::CgsMeta> = reader
+                    .lines()
+                    .enumerate()
+                    .filter_map(|(row, line_result)| match line_result {
+                        Ok(line) => {
+                            let cgs_meta = cgs::process(&line, row);
+                            println!("proc cgs row[{row}] {cgs_meta:?}");
+                            cgs_meta
+                        }
+                        Err(err) => {
+                            eprintln!("failed to read cgs line: {err}");
+                            None
+                        }
+                    })
+                    .collect();
+            }
+            Err(err) => {
+                eprintln!("failed to process cgs file: {err}");
+                return Err(err.to_string());
             }
         },
         None => {
             eprint!("`anim_name` not specified -- full directory processing not yet supported");
-            return Err("`anim_name` not specified -- full directory processing not yet supported".to_string());
+            return Err(
+                "`anim_name` not specified -- full directory processing not yet supported"
+                    .to_string(),
+            );
         }
     }
 
--- src/cgs.rs
+++ src/cgs.rs
@@ -1,6 +1,42 @@
 use std::fs::File;
 use std::io::{self, BufReader};
 
+use crate::cgg;
+
+#[derive(Debug, Default)]
+pub struct PartData {
+    pub anchor: i32,
+    pub x_pos: i32,
+    pub y_pos: i32,
+    pub next_type: i32,
+    pub blend_mode: i32,
+    pub opacity: i32,
+    pub rotate: i32,
+    pub img_x: i32,
+    pub img_y: i32,
+    pub img_width: u32,
+    pub img_height: u32,
+    pub page_id: u32,
+    pub index: usize,
+    pub flip_x: bool,
+    pub flip_y: bool,
+    pub line_index: usize,
+    pub frame_index: usize,
+    pub x: i32,
+    pub y: i32,
+    pub delay: u32,
+}
+
+impl From<cgg::PartData> for PartData {
+    fn from(value: cgg::PartData) -> Self {
+        value.into()
+    }
+}
+
+/// frame_index, x, y, delay
+#[derive(Debug)]
+pub struct CgsMeta(pub usize, pub u32, pub u32, pub i32);
+
 pub fn read_file(unit_id: u32, anim_name: &str, input_path: &str) -> io::Result<BufReader<File>> {
     let file_path = format!("{input_path}/unit_{anim_name}_cgs_{unit_id}.csv");
     println!("[cgs] processing `cgs` file [{file_path}]");
@@ -9,4 +45,27 @@ pub fn read_file(unit_id: u32, anim_name: &str, input_path: &str) -> io::Result<
     let reader = BufReader::new(file);
 
     Ok(reader)
+}
+
+pub fn process(text: &str, row: usize) -> Option<CgsMeta> {
+    let params = text
+        .split(",")
+        .take_while(|s| !s.is_empty())
+        .collect::<Vec<&str>>();
+
+    if params.len() < 2 {
+        return None;
+    }
+
+    match params[..] {
+        [frame_index, x, y, delay] => Some(CgsMeta(
+            frame_index
+                .parse()
+                .expect("frame_index should be numerical value"),
+            x.parse().expect("x should be numerical value"),
+            y.parse().expect("y should be numerical value"),
+            delay.parse().expect("delay should be numerical value"),
+        )),
+        _ => None,
+    }
 }
